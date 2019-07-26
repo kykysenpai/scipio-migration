1bce05221ecfe666279f625156eddcedbf0fabd8
kyrill <kyrill.tircher@centran.be>
2019-07-26 07:48:18 +0200

    fix permissions for static resources ?

--- src/main/java/be/mytcc/scipio/config/WebConfig.java
+++ src/main/java/be/mytcc/scipio/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenInterceptor);
+        registry.addInterceptor(tokenInterceptor).addPathPatterns("/api/*");
     }
 }
