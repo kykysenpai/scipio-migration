fdca6bed140014194786f8510fe5b307ddf04032
kyrill <kyrill.tircher@gmail.com>
2020-01-29 12:08:47 +0100

    change message embed creation location

--- src/main/java/be/mytcc/scipio/spotify/Spotify.java
+++ src/main/java/be/mytcc/scipio/spotify/Spotify.java
@@ -99,14 +99,14 @@ public class Spotify {
     }
 
     private void notifyDiscordOfNewRelease(AlbumRelease albumRelease, AlbumReleaseSubscription subscription) {
-        MessageEmbed messageEmbed = new EmbedBuilder()
-                .setTitle("Listen on Spotify", albumRelease.getLink())
-                .setDescription(getDescription(albumRelease, subscription))
-                .setImage(albumRelease.getImageLink())
-                .setTimestamp(albumRelease.getReleaseDate().toInstant())
-                .build();
-
         subscription.getUsersToNotify().forEach(user -> {
+            MessageEmbed messageEmbed = new EmbedBuilder()
+                    .setTitle("Listen on Spotify", albumRelease.getLink())
+                    .setDescription(getDescription(albumRelease, subscription))
+                    .setImage(albumRelease.getImageLink())
+                    .setTimestamp(albumRelease.getReleaseDate().toInstant())
+                    .build();
+
             PrivateChannel channel = jda.getPrivateChannelById(user.getDiscordId());
             channel.sendMessage(messageEmbed).queue();
             channel.sendMessage(albumRelease.getLink()).queue();
